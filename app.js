f38979553a97fd685f2b72f6f7ada3011e29bed6
AmalAlmutair <asmutairi86@gmail.com>
2022-02-01 18:44:22 +0300

    start

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+const port = 8000;
+// app.get("/data")
+app.listen(port, () => {
+  console.log("Hello Word");
+});
+// module.export = app;
